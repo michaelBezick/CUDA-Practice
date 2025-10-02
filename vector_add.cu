6ec17a16e9d687e17195f86b9182cb2680330240
Michael Bezick <mbezick@purdue.edu>
2025-10-02 16:10:57 -0400

    removed answer

--- vector_add.cu
+++ vector_add.cu
@@ -2,10 +2,6 @@
 #include <cuda_runtime.h>
 
 __global__ void vector_add(const float *a, const float *b, float *c, int n) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < n) {
-        c[idx] = a[idx] + b[idx];
-    }
 }
 
 int main() {
