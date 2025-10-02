91d26edb1dbf6cd63b5d0cf1a39bf767a7c8ccfa
Michael Bezick <mbezick@purdue.edu>
2025-10-02 16:41:45 -0400

    edits

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -3,17 +3,19 @@
 // A CUDA kernel is marked with __global__
 // This runs on the GPU, callable from the CPU
 __global__ void hello_cuda() {
-    printf("Hello from GPU thread (%d,%d)!\n", blockIdx.x, threadIdx.x);
+  printf("Hello from GPU thread (%d,%d)!\n", blockIdx.x, threadIdx.x);
 }
 
 int main() {
-    // Launch kernel with <<<grid, block>>>
-    // Here: 2 blocks, each with 4 threads
-    hello_cuda<<<2, 4>>>();
+  // Launch kernel with <<<grid, block>>>
+  // Here: 2 blocks, each with 4 threads
+  hello_cuda<<<2, 4>>>();
 
-    // Synchronize to wait for GPU to finish
-    cudaDeviceSynchronize();
+  int i = 1 + 1;
 
-    printf("Hello from CPU!\n");
-    return 0;
+  // Synchronize to wait for GPU to finish
+  cudaDeviceSynchronize();
+
+  printf("Hello from CPU!\n");
+  return 0;
 }
